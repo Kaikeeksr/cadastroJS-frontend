5f979932cf57b048620dc82ba4e28663a5dabcb4
Kaike <kaikeksr@outlook.com>
2023-08-15 14:19:52 -0300

    add gender field on funcionario

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,19 @@
 let id_global = 0
 const lista_funcionario = new Set([])
 
+
 const form = document.querySelector("#my-form")
 const nameInput = document.querySelector("#name")
 const cpfInput = document.querySelector("#cpf")
 const emailInput = document.querySelector("#email")
 const telInput = document.querySelector("#tel")
 const paymentInput = document.querySelector("#payment")
-const sectionInput = document.querySelector("#section")
+const departmentInput = document.querySelector("#department")
+const genderInputs = document.querySelectorAll("input[name='gender']")
 const submitButton = document.querySelector("#submit-button")
 
 const errorMessage = document.querySelector(".msg")
+sectionInput.value = "" //inicia o select setor vazio
 
 //máscara CPF
 cpfInput.addEventListener("keypress", () => {
@@ -35,8 +38,12 @@ function cadastro(id_global) {
   const cpf = cpfInput.value
   const email = emailInput.value
   const tel = telInput.value
-  const setor = sectionInput.value
+  const setor = departmentInput.value
   const pagamento = paymentInput.value
+  const selectedGender = () => {
+    let selected = document.querySelector("input[name='gender']:checked").value
+    return selected
+  }
   const funcionario = {
     id: id_global,
     nome: nome,
@@ -44,7 +51,8 @@ function cadastro(id_global) {
     email: email,
     telefone: tel,
     setor: setor,
-    pagamento: pagamento
+    genero: selectedGender(),
+    pagamento: pagamento,
   }
   //criando uma copia do objeto funcionario e adicionando essa copia dentro de lista_funcionario
   lista_funcionario.add(Object.assign({}, funcionario))
@@ -73,5 +81,6 @@ submitButton.addEventListener("click", (e) => {
     telInput.value = ""
     sectionInput.value = ""
     paymentInput.value = ""
+    genderInputs.value = ""
   }
 })
