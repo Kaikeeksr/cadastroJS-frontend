61d1f9113f331229b4b0f5569369b9efa5c703cb
Kaike <kaikeksr@outlook.com>
2023-08-25 10:57:13 -0300

    methods delete and put

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -45,7 +45,44 @@ const employeeAdded = async (employee) => {
   return EmployeeAdded[0]
 }
 
+const updateEmployee = async (employee_id, employee) => {
+  const {
+    employee_name,
+    employee_cpf,
+    employee_email,
+    employee_tel,
+    employee_departament,
+    employee_gender,
+    employee_wage
+  } = employee
+  const query =
+    "UPDATE tbl_employees SET employee_name = ?, employee_cpf = ?, employee_email = ?, employee_tel = ?, employee_departament = ?," +
+    "employee_gender = ?, employee_wage = ? WHERE employee_id = ?"
+
+  const updatedEmployee = await connection.execute(query, [
+    employee_id,
+    employee_name,
+    employee_cpf,
+    employee_email,
+    employee_tel,
+    employee_departament,
+    employee_gender,
+    employee_wage
+  ])
+
+  return updatedEmployee
+}
+
+const deleteEmployee = async (employee_id) => {
+  const query = "DELETE FROM tbl_employees WHERE employee_id = ?"
+  const removedEmployee = await connection.execute(query, [employee_id])
+
+  return removedEmployee
+}
+
 module.exports = {
   getAll,
-  employeeAdded
+  employeeAdded,
+  deleteEmployee,
+  updateEmployee
 }
