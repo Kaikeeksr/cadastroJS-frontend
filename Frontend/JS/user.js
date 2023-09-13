f9ac3c3174002da1e89bb1f695519cc9eb74cb5e
Kaike <kaikeksr@outlook.com>
2023-09-13 13:01:52 -0300

    user-list page

---
+++ Frontend/JS/user.js
@@ -0,0 +1,26 @@
+const fetchEmployees = async () => {
+  const response = await fetch("http://localhost:3333/user")
+  return response.json()
+}
+
+console.log(fetchEmployees())
+fetchEmployees().then(function (employees) {
+  let placeholder = document.querySelector("#data-output")
+  let out = ""
+  for (let employee of employees) {
+    out += `
+        <tr>
+          <td>${employee.employee_id}</td>
+          <td>${employee.employee_name}</td>
+          <td>${employee.employee_email}</td>
+          <td>${employee.employee_cpf}</td>
+          <td>${employee.employee_tel}</td>
+          <td>${employee.employee_wage}</td>
+          <td>${employee.employee_gender}</td>
+          <td>${employee.created_at}</td>
+        </tr>  
+    `
+  }
+
+  placeholder.innerHTML = out
+})
