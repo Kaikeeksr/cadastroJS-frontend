5664d9772675ca3b70b4cd4d98ad284e4fb683c4
Kaike <kaikeksr@outlook.com>
2024-08-31 22:54:47 -0300

    add a função de desativar o funcionário

--- Pages/User-list/user.js
+++ Pages/User-list/user.js
@@ -8,12 +8,15 @@ fetchEmployees().then(function (employees) {
     loader.classList.add('loader-hidden')
   })
 
+  // Ordena o array de employees em ordem alfabética pelo nome
+  employees.sort((a, b) => a.e_name.localeCompare(b.e_name))
+
   let placeholder = document.querySelector('#data-output')
   let out = ''
   for (let employee of employees) {
     out += `
-        <tr id="linha-${employee.employee_id}">
-          <td class='coluna_id'>${employee.e_id}</td>
+        <tr id="linha-${employee.e_id}">
+          <td class='coluna_id' style='width: 60px'>${employee.e_id}</td>
           <td class='coluna_nome'>${employee.e_name}</td>
           <td class='coluna_email'>${employee.e_email}</td>
           <td class='coluna_cpf'>${employee.e_cpf}</td>
@@ -22,6 +25,16 @@ fetchEmployees().then(function (employees) {
           <td class='coluna_salario'>${employee.e_wage}</td>
           <td class='coluna_genero'>${employee.e_gender}</td>
           <td class='coluna_dt_cadastro'>${employee.created_at}</td>
+          <td class='coluna_btn' style='width: 68px'>
+          <div class="btn_container">
+            <div class="btn_editar" style="cursor: pointer;" onclick="editEmployee(${employee.e_id})" title="Editar">
+              <i class="fa fa-pencil-square-o" aria-hidden="true" style="color: #6c63ff;"></i>
+            </div>
+            <div class="btn_excluir" style="cursor: pointer;" onclick="deleteEmployee(${employee.e_id}, '${employee.e_name}')" title="Excluir">
+              <i class="fa fa-trash" aria-hidden="true" style="color: rgb(255, 92, 92);"></i>
+            </div>
+          </div>
+          </td>
         </tr>  
     `
   }
@@ -42,3 +55,37 @@ fetchEmployees().then(function (employees) {
     $(this).val(valor.toUpperCase())
   })
 })
+
+async function deleteEmployee(id, name) {
+  const userConfirmed = confirm(
+    `Você tem certeza que deseja desativar o funcionário id: ${id}, ${name}?`
+  )
+
+  if (userConfirmed) {
+    try {
+      const response = await fetch(`http://localhost:3333/user/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: id.toString(),
+      })
+
+      if (response.ok) {
+        alert(`O funcionário id: ${id}, ${name} foi desativado.`)
+      } else {
+        const data = await response.json()
+        alert(`Ocorreu um erro: ${data.error}`)
+      }
+    } catch (error) {
+      console.error(error)
+      alert(`Ocorreu um erro ao tentar acessar o servidor: ${error}`)
+    } finally {
+      location.reload()
+    }
+  }
+}
+
+function editEmployee(id) {
+  console.log(id)
+}
